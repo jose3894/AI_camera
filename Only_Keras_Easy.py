4dc738a4459148c4fc7ca4c93211d63e39f25f75
jose8alcaide <jose_3_8_94@hotmail.com>
2019-09-25 18:50:25 +0200

    Added files

---
+++ Only_Keras_Easy.py
@@ -0,0 +1,49 @@
+import numpy as np
+from keras.engine.saving import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense
+
+# cargamos las 4 combinaciones de las compuertas XOR
+training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+
+# y estos son los resultados que se obtienen, en el mismo orden
+target_data = np.array([[0],[1],[1],[0]], "float32")
+
+model = Sequential()
+model.add(Dense(16, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='mean_squared_error',
+              optimizer='adam',
+              metrics=['binary_accuracy'])
+
+model.fit(training_data, target_data, epochs=1000)
+
+# evaluamos el modelo
+scores = model.evaluate(training_data, target_data)
+
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print (model.predict(training_data).round())
+
+# serializar el modelo a JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serializar los pesos a HDF5
+model.save_weights("model.h5")
+print("Modelo Guardado!")
+
+# mas tarde...
+
+# cargar json y crear el modelo
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# cargar pesos al nuevo modelo
+loaded_model.load_weights("model.h5")
+print("Cargado modelo desde disco.")
+
+# Compilar modelo cargado y listo para usar.
+loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+
