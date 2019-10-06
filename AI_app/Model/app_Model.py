8b7608c1f4d1ecbb66feb38af8b6ea9c1df7f6d3
jose8alcaide <jose_3_8_94@hotmail.com>
2019-10-06 12:38:00 +0200

    new dockerfile new test model

--- AI_app/Model/app_Model.py
+++ AI_app/Model/app_Model.py
@@ -33,18 +33,8 @@ with open("model.json", "w") as json_file:
 model.save_weights("model.h5")
 print("Modelo Guardado!")
 
-# mas tarde...
 
-# cargar json y crear el modelo
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# cargar pesos al nuevo modelo
-loaded_model.load_weights("model.h5")
-print("Cargado modelo desde disco holaaaaaa")
 
-# Compilar modelo cargado y listo para usar.
-loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+
 
 print("=========== MODEL ===========")
--- AI_app/Testing/app_Test.py
+++ AI_app/Testing/app_Test.py
@@ -1,3 +1,15 @@
+from keras.engine.saving import model_from_json
 
+# cargar json y crear el modelo
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# cargar pesos al nuevo modelo
+loaded_model.load_weights("model.h5")
+print("Cargado modelo desde disco")
+
+# Compilar modelo cargado y listo para usar.
+loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
 
 print("=========== TEST ===========")
