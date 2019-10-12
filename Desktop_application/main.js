8b9f94f7559093921ee2ca2adfa8395d8c7f114e
jose8alcaide <jose_3_8_94@hotmail.com>
2019-10-12 18:24:25 +0200

    add desktop app

--- Desktop_application/main.js
+++ Desktop_application/main.js
@@ -38,7 +38,7 @@ function loadCamera(){
 function createAboutWindow(){
     newAboutWindow = new BrowserWindow({
         icon: 'assets/img/lens.png',
-        width: 400,
+        width: 600,
         height: 300,
         title: 'About'
     });
