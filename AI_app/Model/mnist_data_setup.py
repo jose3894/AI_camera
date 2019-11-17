00b7dc5f16eb3415773b870fc265ded463cc5e8e
jose8alcaide <jose_3_8_94@hotmail.com>
2019-11-17 20:26:38 +0100

    Modify cluster spark

--- AI_app/Model/mnist_data_setup.py
+++ AI_app/Model/mnist_data_setup.py
@@ -1,65 +1,92 @@
-# Copyright 2017 Yahoo Inc.
-# Licensed under the terms of the Apache 2.0 license.
-# Please see LICENSE file in the project root for terms.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-if __name__ == "__main__":
+def main_fun(args, ctx):
+  import tensorflow_datasets as tfds
+  import tensorflow as tf
+  tfds.disable_progress_bar()
+
+  strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
+  BUFFER_SIZE = args.buffer_size
+  BATCH_SIZE = args.batch_size
+  NUM_WORKERS = args.cluster_size
+
+  # Scaling MNIST data from (0, 255] to (0., 1.]
+  def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+  datasets, info = tfds.load(name='mnist',
+                             with_info=True,
+                             as_supervised=True)
+
+  train_datasets_unbatched = datasets['train'].repeat().map(scale).shuffle(BUFFER_SIZE)
+
+  def build_and_compile_cnn_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(
+        loss=tf.keras.losses.sparse_categorical_crossentropy,
+        optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+        metrics=['accuracy'])
+    return model
+
+  # single node
+  # single_worker_model = build_and_compile_cnn_model()
+  # single_worker_model.fit(x=train_datasets, epochs=3)
+
+  # Here the batch size scales up by number of workers since
+  # `tf.data.Dataset.batch` expects the global batch size. Previously we used 64,
+  # and now this becomes 128.
+  GLOBAL_BATCH_SIZE = BATCH_SIZE * NUM_WORKERS
+  train_datasets = train_datasets_unbatched.batch(GLOBAL_BATCH_SIZE)
+
+  # this fails
+  # callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath=args.model_dir)]
+  tf.io.gfile.makedirs(args.model_dir)
+  filepath = args.model_dir + "/weights-{epoch:04d}"
+  callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath=filepath, verbose=1, save_weights_only=True)]
+
+  with strategy.scope():
+    multi_worker_model = build_and_compile_cnn_model()
+  multi_worker_model.fit(x=train_datasets, epochs=args.epochs, steps_per_epoch=args.steps_per_epoch, callbacks=callbacks)
+
+  if ctx.job_name == 'chief':
+    from tensorflow_estimator.python.estimator.export import export_lib
+    export_dir = export_lib.get_timestamped_export_dir(args.export_dir)
+    tf.keras.experimental.export_saved_model(multi_worker_model, export_dir)
+    # multi_worker_model.save(args.model_dir, save_format='tf')
+
+
+if __name__ == '__main__':
   import argparse
-
   from pyspark.context import SparkContext
   from pyspark.conf import SparkConf
-  import tensorflow as tf
-  import tensorflow_datasets as tfds
+  from tensorflowonspark import TFCluster
+
+  sc = SparkContext(conf=SparkConf().setAppName("mnist_keras"))
+  executors = sc._conf.get("spark.executor.instances")
+  num_executors = int(executors) if executors is not None else 1
 
   parser = argparse.ArgumentParser()
-  parser.add_argument("--num_partitions", help="Number of output partitions", type=int, default=10)
-  parser.add_argument("--output", help="HDFS directory to save examples in parallelized format", default="data/mnist")
+  parser.add_argument("--batch_size", help="number of records per batch", type=int, default=64)
+  parser.add_argument("--buffer_size", help="size of shuffle buffer", type=int, default=10000)
+  parser.add_argument("--cluster_size", help="number of nodes in the cluster", type=int, default=3)
+  parser.add_argument("--epochs", help="number of epochs", type=int, default=3)
+  parser.add_argument("--model_dir", help="path to save model/checkpoint", default="mnist_model")
+  parser.add_argument("--export_dir", help="path to export saved_model", default="mnist_export")
+  parser.add_argument("--steps_per_epoch", help="number of steps per epoch", type=int, default=469)
+  parser.add_argument("--tensorboard", help="launch tensorboard process", action="store_true")
 
   args = parser.parse_args()
   print("args:", args)
 
-  sc = SparkContext(conf=SparkConf().setAppName("mnist_data_setup"))
-
-  mnist, info = tfds.load('mnist', with_info=True)
-  print(info.as_json)
-
-  # convert to numpy, then RDDs
-  mnist_train = tfds.as_numpy(mnist['train'])
-  mnist_test = tfds.as_numpy(mnist['test'])
-
-  train_rdd = sc.parallelize(mnist_train, args.num_partitions).cache()
-  test_rdd = sc.parallelize(mnist_test, args.num_partitions).cache()
-
-  # save as CSV (label,comma-separated-features)
-  def to_csv(example):
-    return str(example['label']) + ',' + ','.join([str(i) for i in example['image'].reshape(784)])
-
-  train_rdd.map(to_csv).saveAsTextFile(args.output + "/csv/train")
-  test_rdd.map(to_csv).saveAsTextFile(args.output + "/csv/test")
-
-  # save as TFRecords (numpy vs. PNG)
-  # note: the MNIST tensorflow_dataset is already provided as TFRecords but with a PNG bytes_list
-  # this export format is less-efficient, but easier to work with later
-  def to_tfr(example):
-    ex = tf.train.Example(
-      features=tf.train.Features(
-        feature={
-          'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[example['label'].astype("int64")])),
-          'image': tf.train.Feature(int64_list=tf.train.Int64List(value=example['image'].reshape(784).astype("int64")))
-        }
-      )
-    )
-    return (bytearray(ex.SerializeToString()), None)
-
-  train_rdd.map(to_tfr).saveAsNewAPIHadoopFile(args.output + "/tfr/train",
-                                               "org.tensorflow.hadoop.io.TFRecordFileOutputFormat",
-                                               keyClass="org.apache.hadoop.io.BytesWritable",
-                                               valueClass="org.apache.hadoop.io.NullWritable")
-  test_rdd.map(to_tfr).saveAsNewAPIHadoopFile(args.output + "/tfr/test",
-                                               "org.tensorflow.hadoop.io.TFRecordFileOutputFormat",
-                                               keyClass="org.apache.hadoop.io.BytesWritable",
-                                               valueClass="org.apache.hadoop.io.NullWritable")
+  cluster = TFCluster.run(sc, main_fun, args, args.cluster_size, num_ps=0, tensorboard=args.tensorboard, input_mode=TFCluster.InputMode.TENSORFLOW, master_node='chief')
+  cluster.shutdown()
