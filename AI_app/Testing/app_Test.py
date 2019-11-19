aa3db44bb431ec970a67f5470a9749689fd6ae9d
jose8alcaide <jose_3_8_94@hotmail.com>
2019-11-19 22:56:00 +0100

    change AI

--- AI_app/Testing/app_Test.py
+++ AI_app/Testing/app_Test.py
@@ -1,22 +1,54 @@
+import tensorflow as tf
 import numpy as np
-from keras.engine.saving import model_from_json
+import tensorflow.keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras import backend as K
 
-# cargamos las 4 combinaciones de las compuertas XOR
-training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+batch_size=100
+num_classes=10
+epochs=6
 
-# cargar json y crear el modelo
-json_file = open('Model/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# cargar pesos al nuevo modelo
-loaded_model.load_weights("Model/model.h5")
-print("Cargado modelo desde disco")
+filas,columnas= 28,28
 
-# Compilar modelo cargado y listo para usar.
-loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+(xt,yt),(xtest,ytest)= mnist.load_data()
 
-# Predict
-print (loaded_model.predict(training_data).round())
+xt=xt.reshape(xt.shape[0],filas,columnas,1)
+xtest=xtest.reshape(xtest.shape[0],filas,columnas,1)
 
-print("=========== TEST ===========")
+xt=xt.astype('float32')
+xtest=xtest.astype('float32')
+
+xt=xt/255
+xtest=xtest/255
+
+yt=tensorflow.keras.utils.to_categorical(yt,num_classes)
+ytest=tensorflow.keras.utils.to_categorical(ytest,num_classes)
+
+# modelo=Sequential()
+# modelo.add(Conv2D(64,kernel_size=(3,3),activation='relu',input_shape=(28,28,1)))
+# modelo.add(Conv2D(128,kernel_size=(3,3),activation='relu',input_shape=(28,28,1)))
+# modelo.add(MaxPooling2D(pool_size=(2,2)))
+# modelo.add(Flatten())
+# modelo.add(Dense(68))
+# modelo.add(Dropout(0.25))
+# modelo.add(Dense(20))
+# modelo.add(Dropout(0.25))
+# modelo.add(Dense(num_classes,activation='softmax'))
+
+
+modelo=Sequential()
+modelo.add(Flatten(input_shape=(28,28,1)))
+modelo.add(Dense(68,activation='relu'))
+modelo.add(Dense(20,activation='relu'))
+modelo.add(Dense(num_classes,activation='softmax'))
+modelo.summary()
+
+modelo.compile(loss=tensorflow.keras.losses.categorical_crossentropy,optimizer=tensorflow.keras.optimizers.Adam(),metrics=['categorical_accuracy'])
+
+modelo.fit(xt,yt,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(xtest,ytest))
+
+puntuacion=modelo.evaluate(xtest,ytest,verbose=1)
+
+print(puntuacion)
