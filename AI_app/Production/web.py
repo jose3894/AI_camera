e4da1531cee314b6015bdb08f7e261c450729c35
jose8alcaide <jose_3_8_94@hotmail.com>
2019-12-21 20:53:28 +0100

    Modify production app

--- AI_app/Production/web.py
+++ AI_app/Production/web.py
@@ -1,14 +1,55 @@
 from flask import Flask, render_template, Response
 from camera import CameraStream
 import cv2
-from object_detection import ObjectDetection
+from os.path import join
+#from tensorflow.lite.python.interpreter import Interpreter
+from tflite_runtime.interpreter import Interpreter
 
+import numpy as np
 
 app = Flask(__name__)
 
 cap = CameraStream().start()
 
-obj = ObjectDetection()
+MODEL_NAME = r'/app/Production/Sample_TF_model'
+GRAPH_NAME = 'detect.tflite'
+LABELMAP_NAME = 'labelmap.txt'
+min_conf_threshold = 0.5
+resolution = '640x480'
+resW, resH = resolution.split('x')
+imW, imH = int(resW), int(resH)
+
+# Path to .tflite file, which contains the model that is used for object detection
+PATH_TO_CKPT = join(MODEL_NAME, GRAPH_NAME)
+
+# Path to label map file
+PATH_TO_LABELS = join(MODEL_NAME, LABELMAP_NAME)
+
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+if labels[0] == '???':
+    del(labels[0])
+
+interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
 
 @app.route('/')
 def index():
@@ -19,12 +60,67 @@ def index():
 def gen_frame():
     """Video streaming generator function."""
     while cap:
-        frame = cap.read()
-        frame1 = frame
-        frame = obj.predict(frame)
-        convert = cv2.imencode('.jpg', frame1)[1].tobytes()
+        # Start timer (for calculating frame rate)
+        #t1 = cv2.getTickCount()
+
+        frame1 = cap.read()
+
+        # Acquire frame and resize to expected shape [1xHxWx3]
+        frame = frame1.copy()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (width, height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
+
+        # Perform the actual detection by running the model with the image as input
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+
+        # Retrieve detection results
+        boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
+        classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
+        scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
+        # num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1, (boxes[i][0] * imH)))
+                xmin = int(max(1, (boxes[i][1] * imW)))
+                ymax = int(min(imH, (boxes[i][2] * imH)))
+                xmax = int(min(imW, (boxes[i][3] * imW)))
+
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
+                # Draw label
+                object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
+                              (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
+                              cv2.FILLED)  # Draw white box to put label text in
+                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
+                            2)  # Draw label text
+
+        # Draw framerate in corner of frame
+        cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0),
+                    2, cv2.LINE_AA)
+
+        convert = cv2.imencode('.jpg', frame)[1].tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + convert + b'\r\n') # concate frame one by one and show result
+        """
+        # Calculate framerate
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / freq
+        frame_rate_calc = 1 / time1
+        """
 
 @app.route('/video_feed')
 def video_feed():
