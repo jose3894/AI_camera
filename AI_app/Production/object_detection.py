1f982da6e00fa4dd0c4f41e43ae10d6529f97c77
jose8alcaide <jose_3_8_94@hotmail.com>
2019-12-21 20:13:22 +0100

    Modify production app

---
+++ AI_app/Production/object_detection.py
@@ -0,0 +1,118 @@
+import cv2
+import numpy as np
+import yaml
+from os.path import join
+from tflite_runtime.interpreter import Interpreter
+
+
+# START Open configuration files
+settings_file = {}
+SETTINGS_FILE = "settings.yaml"
+
+try:
+    with open(SETTINGS_FILE, 'r') as file:
+        try:
+            settings_file = yaml.safe_load(file)
+        except yaml.YAMLError as error:
+            print(error)
+except IOError:
+    print("WARNING, no settings.yaml in directory")
+    raise
+
+
+# END Open configuration files
+
+
+class ObjectDetection:
+    def __init__(self):
+        self.imW = 0
+        self.imH = 0
+        self.labels = []
+        self.interpreter
+        self.input_details
+        self.output_details
+        self.height = 0
+        self.width = 0
+        self.floating_model
+        self.input_mean = 0
+        self.input_std = 0
+
+    def initialize_model(self):
+        resW, resH = settings_file['RESOLUTION'].split('x')
+        self.imW, self.imH = int(resW), int(resH)
+
+        # Path to .tflite file, which contains the model that is used for object detection
+        PATH_TO_CKPT = join(settings_file['MODEL_NAME'], settings_file['GRAPH_NAME'])
+
+        # Path to label map file
+        PATH_TO_LABELS = join(settings_file['MODEL_NAME'], settings_file['LABELMAP_NAME'])
+
+        # Load the label map
+        with open(PATH_TO_LABELS, 'r') as f:
+            self.labels = [line.strip() for line in f.readlines()]
+
+        if self.labels[0] == '???':
+            del (self.labels[0])
+
+        self.interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+        self.interpreter.allocate_tensors()
+
+        # Get model details
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+        self.height = self.input_details[0]['shape'][1]
+        self.width = self.input_details[0]['shape'][2]
+
+        self.floating_model = (self.input_details[0]['dtype'] == np.float32)
+
+        self.input_mean = 127.5
+        self.input_std = 127.5
+
+        # Initialize frame rate calculation
+        #frame_rate_calc = 1
+        #freq = cv2.getTickFrequency()
+
+    def predict(self, frame):
+        # Acquire frame and resize to expected shape [1xHxWx3]
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (self.width, self.height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if self.floating_model:
+            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
+
+        # Perform the actual detection by running the model with the image as input
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.invoke()
+
+        # Retrieve detection results
+        boxes = self.interpreter.get_tensor(self.output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
+        classes = self.interpreter.get_tensor(self.output_details[1]['index'])[0]  # Class index of detected objects
+        scores = self.interpreter.get_tensor(self.output_details[2]['index'])[0]  # Confidence of detected objects
+        # num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if ((scores[i] > self.min_conf_threshold) and (scores[i] <= 1.0)):
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1, (boxes[i][0] * self.imH)))
+                xmin = int(max(1, (boxes[i][1] * self.imW)))
+                ymax = int(min(self.imH, (boxes[i][2] * self.imH)))
+                xmax = int(min(self.imW, (boxes[i][3] * self.imW)))
+
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
+                # Draw label
+                object_name = self.labels[int(classes[i])]  # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
+                              (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
+                              cv2.FILLED)  # Draw white box to put label text in
+                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
+                            2)  # Draw label text
+        return frame
