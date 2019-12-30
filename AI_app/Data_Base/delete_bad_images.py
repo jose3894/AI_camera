467ad2eb4eb91e8c9d7ced287f40b7be74e85e3c
jose8alcaide <jose_3_8_94@hotmail.com>
2019-12-30 20:02:42 +0100

    Added script delete bad images

---
+++ AI_app/Data_Base/delete_bad_images.py
@@ -0,0 +1,16 @@
+from os import walk, remove
+from os.path import join
+import cv2
+
+PATH = r'/home/jose8alcaide/Documentos/AI_camera/AI_app/Model/db_images2'
+counter = 0
+
+for path, dirs, files in walk(PATH):
+        for file in files:
+            if cv2.imread(join(path, file), cv2.IMREAD_UNCHANGED) is None:
+                counter += 1
+                remove(join(path, file))
+        print(str(counter) + ' images deletes in folder ' + path)
+        counter = 0
+
+
